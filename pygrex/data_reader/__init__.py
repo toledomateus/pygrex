cf22e45594513ab9b1b7fbbae0b3d3200ecf7949
Mateus Toledo <mateustsleao@gmail.com>
2025-09-17 13:31:13 +0200

    fix init datareader folder

--- pygrex/data_reader/__init__.py
+++ pygrex/data_reader/__init__.py
@@ -1,5 +1,11 @@
 from .data_reader import DataReader
 from .user_item_rating_dataset import UserItemRatingDataset
 from .group_interaction_handler import GroupInteractionHandler
+from .user_item_dict import UserItemDict
 
-__all__ = ["DataReader", "UserItemRatingDataset", "GroupInteractionHandler"]
+__all__ = [
+    "DataReader",
+    "UserItemRatingDataset",
+    "GroupInteractionHandler",
+    "UserItemDict",
+]
